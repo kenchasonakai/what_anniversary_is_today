f733a204d1628c549620da69a834cdd3c5affe14
nakai <nakai@nakainoMacBook-Pro.local>
2021-07-25 19:05:47 +0900

    fix: rubocopによるLint修正

--- app/models/user_anniversary.rb
+++ app/models/user_anniversary.rb
@@ -1,4 +1,4 @@
 class UserAnniversary < ApplicationRecord
-  validates :date, presence: true, length: { maximum: 4}
+  validates :date, presence: true, length: { maximum: 4 }
   validates :name, presence: true, length: { maximum: 255 }
 end
