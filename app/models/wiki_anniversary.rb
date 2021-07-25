be7e5ab5520d5daccd0441004be2f93f5e0b5516
nakai <nakai@nakainoMacBook-Pro.local>
2021-07-25 10:56:08 +0900

    add: wiki_anniversaryモデルの追加

---
+++ app/models/wiki_anniversary.rb
@@ -0,0 +1,4 @@
+class WikiAnniversary < ApplicationRecord
+  validates :date, presence: true, length: { maximum: 4}
+  validates :name, presence: true, length: { maximum: 30000 }
+end
---
+++ spec/models/wiki_anniversary_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe WikiAnniversary, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
